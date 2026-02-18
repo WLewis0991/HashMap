970a77a3e3c80303219176c8d6cea9c0abd7ff6b
wlewis0991 <wlewis1990@gmail.com>
2026-02-18 13:14:10 -0800

    Values and keys functions added to produce all values and keys in the hash

--- index.js
+++ index.js
@@ -20,48 +20,85 @@ class HashMap {
 		const index = this.hash(key);
 
 		if (index < 0 || index >= this.buckets.length) {
-  			throw new Error("Trying to access index out of bounds");
+			throw new Error("Trying to access index out of bounds");
 		}
-		
-		if (this.buckets[index] === undefined){
-		this.buckets[index]= new LinkedList();
-		this.buckets[index].append(key, value);
-		this.size += 1;
+
+		if (this.buckets[index] === undefined) {
+			this.buckets[index] = new LinkedList();
+			this.buckets[index].append(key, value);
+			this.size += 1;
 		} else {
-			this.buckets[index].append(key, value)
+			this.buckets[index].append(key, value);
 			this.size += 1;
 		}
 	}
 
-	get(key){
+	get(key) {
 		const index = this.hash(key);
 
 		if (index < 0 || index >= this.buckets.length) {
-  			throw new Error("Trying to access index out of bounds");
+			throw new Error("Trying to access index out of bounds");
 		}
-		if (this.buckets[index] === undefined){
-			return null
+		if (this.buckets[index] === undefined) {
+			return null;
 		} else {
-			return this.buckets[index].getNode(key)
+			return this.buckets[index].getNode(key);
 		}
 	}
 
-	has(key){
+	has(key) {
 		const index = this.hash(key);
 
 		if (index < 0 || index >= this.buckets.length) {
+			throw new Error("Trying to access index out of bounds");
+		}
+		if (this.buckets[index] === undefined) {
+			return false;
+		} else {
+			return this.buckets[index].contains(key);
+		}
+	}
+
+	keys() {
+		const result = [];
+		for (let i = 0; i < this.buckets.length; i++) {
+			const bucket = this.buckets[i];
+			if (!bucket) continue;
+			result.push(...bucket.getKeys());
+		}
+		return result;
+	}
+
+
+	values() {
+		const result = [];
+		for (let i = 0; i < this.buckets.length; i++) {
+			const bucket = this.buckets[i];
+			if (!bucket) continue;
+			result.push(...bucket.getValues());
+		}
+		return result;
+	}
+
+
+	/*
+	remove(key){
+		const index = this.hash(key)
+		
+		if (index < 0 || index >= this.buckets.length) {
   			throw new Error("Trying to access index out of bounds");
 		}
-		if (this.buckets[index] === undefined){
-			return false
-		}else {
-			return this.buckets[index].contains(key)
+		if (this.bucket[index] === undefined){
+			return "Key not found"
+		} else {
+			return this.buckets[index].removeAt(key)
 		}
 
 	}
+*/
 
-	length(){
-		return this.size
+	length() {
+		return this.size;
 	}
 
 	clear() {
@@ -75,8 +112,6 @@ class HashMap {
 class LinkedList {
 	constructor() {
 		this.head = null;
-
-
 	}
 
 	append(key, value) {
@@ -117,7 +152,56 @@ class LinkedList {
 			}
 		}
 		return false;
-	}	
+	}
+
+	getKeys() {
+		if (this.head == null) {
+			return "";
+		} else {
+			const keys = [];
+			let current = this.head;
+			while (current != null) {
+				keys.push(current.key);
+				current = current.nextNode;
+			}
+			return keys;
+		}
+	}
+
+
+	getValues() {
+		if (this.head == null) {
+			return "";
+		} else {
+			const values = [];
+			let current = this.head;
+			while (current != null) {
+				values.push(current.value);
+				current = current.nextNode;
+			}
+			return values;
+		}
+	}
+
+	/*
+	removeAt(key) {
+
+		let current = this.head;
+		
+		if (current.key === key){
+			this.head = current.nextNode
+			return console.log("Key Removed")
+		}
+		while (current != null) {
+			current = current.nextNode;
+			count++;
+		}
+		if (current == null || current.nextNode == null) {
+			return RangeError("Number out of index range");
+		}
+		current.nextNode = current.nextNode.nextNode;
+	}
+		*/
 }
 
 class Node {
@@ -130,24 +214,27 @@ class Node {
 
 const test = new HashMap();
 
-test.set('apple', 'red')
-test.set('banana', 'yellow')
-test.set('carrot', 'orange')
-test.set('dog', 'brown')
-test.set('elephant', 'gray')
-test.set('frog', 'green')
-test.set('grape', 'purple')
-test.set('hat', 'black')
-test.set('ice cream', 'white')
-test.set('jacket', 'blue')
-test.set('kite', 'pink')
-test.set('lion', 'golden')
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+test.set("kite", "pink");
+test.set("lion", "golden");
 
 console.log(test);
-setTimeout(() =>{
+setTimeout(() => {
 	console.log(test.has("monkey"));
-},2000)
+}, 2000);
 
-setTimeout(() =>{
+setTimeout(() => {
 	console.log(test);
-}, 3000)
+}, 3000);
+
+console.log(test.keys())
+console.log(test.values())
